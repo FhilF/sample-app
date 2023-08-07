6ca80f8b527741a74a12f8dc9b8bd2cff87f324e
FhilF <52390799+FhilF@users.noreply.github.com>
2023-08-07 15:44:14 +0800

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  return res.send("Server works");
+  return res.send("Server works yes");
 });
 
 app.listen(PORT, () => {
